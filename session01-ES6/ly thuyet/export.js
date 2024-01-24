983056db6944190e045ed1e2d2a44a003ab7fa7b
vanh2712 <vanhtb1998@gmail.com>
2024-01-24 21:28:29 +0700

    add file

---
+++ session01-ES6/ly thuyet/export.js
@@ -0,0 +1,7 @@
+function sayHello() {
+  console.log("import/export");
+}
+let b = 5;
+let fullName = "van anh";
+export { b, fullName };
+export default sayHello;
---
+++ session01-ES6/ly thuyet/index.js
@@ -0,0 +1,5 @@
+import sayHello from "./export.js";
+import { fullName as name } from "./export.js";
+
+console.log("sayhello", sayHello());
+console.log(name);
